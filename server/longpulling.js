b9377067c375423d5419c0d0816d3d71f532cb36
isrzian <shadow-sergej@ya.ru>
2022-02-14 17:51:44 +0700

    long pulling server

---
+++ server/longpulling.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+const {EventEmitter} = require('events')
+
+const PORT = 5000
+
+const emitter = new EventEmitter()
+
+const app = express()
+
+app.use(cors())
+
+app.get('get-messages', (req, res) => {
+	emitter.once('newMessage', (message) => {
+		res.json(message)
+	})
+})
+
+app.post('new-messages', (req, res) => {
+	const message = req.body
+	emitter.emit('newMessage', message)
+	res.status(200)
+})
+
+app.listen(PORT, () => console.log(`Server has been started on ${PORT} port`))
